14debdb3bb16e752704878deb4b922bb86d2c3b2
Ricky <yuen.ricky@hotmail.com>
2021-10-08 09:13:59 -0400

    deploy script uses config based on network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,26 @@
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
+
+async function deploy_lottery() {
+  const Lottery = await ethers.getContractFactory("Lottery");
+
+  const { priceFeedAddress, vrfCoordinator, linkToken, fee, keyhash } =
+    network.config;
+
+  const lottery = await Lottery.deploy(
+    priceFeedAddress,
+    vrfCoordinator,
+    linkToken,
+    fee,
+    keyhash
+  );
+
+  return lottery;
+}
 
 async function main() {
-  const Lottery = await ethers.getContractFactory("Lottery");
-  const lottery = await Lottery.deploy();
-
+  const lottery = await deploy_lottery();
   console.log("Lottery address:", lottery.address);
+  console.log("entrance: ", (await lottery.getEntranceFee()).toString());
 }
 
 main()
